b17770f1e48d57a26d1185bc0e951f2f3e5a8e7a
Andrei <10406976+rsvts@users.noreply.github.com>
2019-10-09 11:33:42 +0200

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function zeros(expression) {
-  // your solution
+  
+    let counter2 =
+        counter5 =
+        tmp = 0;
+
+    expression.split('*').forEach((item) => {
+
+        item.replace(/([0-9]+)(!+)/g, (...args) => {
+
+            while (args[1] > 1) {
+
+                tmp = args[1];
+
+                while (tmp % 2 == 0) {
+
+                    counter2++;
+                    tmp /= 2;
+                }
+
+                while (tmp % 5 == 0) {
+
+                    counter5++;
+                    tmp /= 5;
+                }
+
+                args[1] -= args[2].length;
+
+            }
+        })
+    });
+
+    return Math.min(counter2, counter5);
 }
